65d53bd92a16221b1259a96ad2cda77e17afa8cf
Luis <luis@blooz.pe>
2022-10-09 00:55:45 -0500

    class 19: date management

---
+++ 19_date_management-part1.py
@@ -0,0 +1,23 @@
+# class 19: date management
+import datetime
+
+
+my_datetime = datetime.datetime.now()
+
+print(f'Now: {my_datetime}')
+
+my_day = datetime.date.today()
+
+print(f'Year: {my_day.year}')
+print(f'Month: {my_day.month}')
+print(f'Day: {my_day.day}')
+
+# Formato de fechas
+my_str1 = my_datetime.strftime('%d/%m/%Y')
+print(f'Formato LATAM: {my_str1}')
+
+my_str2 = my_datetime.strftime('%m/%d/%Y')
+print(f'Formato USA: {my_str2}')
+
+my_str3 = my_datetime.strftime(f'Estamos en el año %Y')
+print(f'Formato random: {my_str3}')
---
+++ 19_date_management-part2.py
@@ -0,0 +1,31 @@
+# class 19: date management
+'''
+Si quieren aplicar Python para análisis de datos, es fundamental que 
+se domine el manejo de fechas.
+
+La librería "datetime" tiene dos funciones muy utilizadas para estos casos:
+
+1- timedelta: retorna un delta de tiempo, es de ayuda en situaciones donde
+necesitamos restar o sumar tiempos (ejemplo: 1 día a cada fecha o 20 minutos cada fecha hora)
+
+2- strptime: recibe un str y retorna un objeto datetime, esto se usa para 
+validar formatos de fecha capturando un ValueError cuando se trabaja con archivos y 
+queremos validar formatos de fechas con simple lambda podemos aplicar formato a 
+todo el dataset y capturas los errores antes de operar con ellos o guardar en la db.
+'''
+import datetime
+
+
+delta_hr = datetime.timedelta(hours=3)
+print("delta in hours: ", delta_hr)
+
+delta_min = datetime.timedelta(minutes=20)
+print("delta in minutes: ", delta_min)
+
+delta_sec = datetime.timedelta(seconds=30)
+print("delta in seconds: ", delta_sec)
+
+# si no se cumple con el formato lanzara un ValueError
+date_time = datetime.datetime.strptime('2022-10-09 00:40:00' , '%Y-%m-%d %H:%M:%S')
+result = date_time + delta_min
+print(result)
